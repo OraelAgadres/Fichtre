6542c797787f8adbc34e3e4663de5cd247a064dd
OraelAgadres <orael.agadres@gmail.com>
2020-03-20 22:38:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new.Discord.Client({DisableEveryone: true})
 
 bot.on("ready", async () => {
-	bot.user.setActivity('Pute')
+	bot.user.setActivity('Pie')
 });
 
 bot.login(process.env.BOT_TOKEN)
