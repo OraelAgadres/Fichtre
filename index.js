a55a2855255f83303d378b7debe750f901ca178c
OraelAgadres <orael.agadres@gmail.com>
2020-03-20 22:42:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({DisableEveryone: true})
 
 bot.on("ready", async () => {
+	console.log("Hi i'm ready to kill some pussy")
+
 	bot.user.setActivity('Pie')
 });
 
