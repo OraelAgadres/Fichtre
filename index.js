ef95c269c24f0c089d96393e046121340c9311c4
OraelAgadres <orael.agadres@gmail.com>
2020-03-20 23:26:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,12 @@ bot.on("message", async (msg) => {
 	if (msg.content.startsWith(config.prefix) && !msg.author.bot) {
 		var cmdArray = msg.content.substring(2).split(" ")
 		var cmd = cmdArray[0]
-
 		var args = cmdArray.slice(1)
 
-		msg.channel.send(cmd)
-		for (var i = 0, c = args.length; i < c; i++) {
-			msg.channel.send(args[i])
+		if (cmd == "ping") {
+			msg.channel.send("pong :joy:")
 		}
+
 	}
 });
 
